bb9350b16364cc26f8e8aa1f644106ceccbdd52f
SlimMoussa <slimmoussa2@gmail.com>
2024-10-30 08:56:06 +0100

    first commit

---
+++ src/test/java/Helper/Config.java
@@ -0,0 +1,15 @@
+package Helper;
+
+import org.openqa.selenium.WebDriver;
+
+public class Config {
+	
+	public static WebDriver driver;
+	public static void Confchrome () {
+		System.setProperty("webdriver.edge.driver", "c:/msdedgedriver.exe");
+		
+	}
+public static void maximisewindow () {
+	
+}
+}
